05ab06183978071da79c1074723f85018fc444c9
TigranZakk <zakharyantigran27@gmail.com>
2021-06-19 22:02:38 +0400

    start

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ const {
 } = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
+    htmlreplace = require('gulp-html-replace'),
     uglify = require('gulp-uglify-es').default,
     cleancss = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
     cleandist = require('del'),
     browserSync = require('browser-sync').create(),
@@ -17,42 +19,47 @@ const {
 // function
 function browsersync() {
     browserSync.init({
-            proxy: 'localhost/portfolio-master',
-            baseDir: "./app",
+            proxy: 'localhost/shop-master/app',
             port: 5000
     })
 }
 
-function style() {
+function scssCompile() {
     return src("app/scss/*.scss")
         .pipe(sass())
+        .pipe(concat("style.css"))
         .pipe(cleancss({
             keepBreaks: false
-        }))
-        .pipe(rename({
-            extname: '.min.css'
         }))
         .pipe(dest("app/css"))
         .pipe(browserSync.stream());
 }
-
-function script() {
-    return src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+function styleBuild() {
+    return src('app/*css/**/*.css')
+        .pipe(cleancss({
+            keepBreaks: false
+        }))
+        .pipe(rename({
+            basename: "bundle",
+            extname: '.min.css'
+        }))
+        .pipe(dest("dist/css"))
+}
+function scriptBuild() {
+    return src(['app/*js/**/*.js'])
         .pipe(uglify())
         .pipe(rename({
+            basename: "bundle",
             extname: '.min.js'
         }))
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream());
-
+        .pipe(dest('dist'))
 }
 
 function del() {
     return cleandist('dist');
 }
-
 function image() {
-    return src(['app/img/**/*'])
+    return src(['app/*img/**/*'])
         .pipe(imagemin([
             imagemin.gifsicle({
                 interlaced: true
@@ -74,24 +81,29 @@ function image() {
                 ]
             })
         ]))
-        .pipe(dest('app/img'))
+        .pipe(dest('dist/img'))
 }
 
 function watching() {
-    watch('app/scss/**/*.scss', style)
-    watch(['app/js/**/*.js', '!app/js/**/*.min.js'], script)
-    watch("app/*.html").on('change', browserSync.reload);
-    watch("app/*.php").on('change', browserSync.reload);
+    watch("app/scss/**/*", scssCompile);
+    watch("app/**/*").on('change', browserSync.reload);
 }
-
+function html(){
+    return src("app/index.html")
+    .pipe(htmlreplace({
+        js: 'js/bundle.min.js',
+        css: "css/bundle.min.css"
+    }))
+    .pipe(dest("dist/"))
+}
 function build() {
-    return src(['app/*.html', 'app/*.php', 'app/*.txt', 'app/*.xml', 'app/*img/**/*', 'app/*js/**/*.min.js', 'app/*css/**/*.min.css', 'app/*font/**/*',"app/*conf**/*"])
+    return src(['app/*.php', 'app/*.txt', 'app/*.xml',"app/*conf**/*"])
         .pipe(dest('dist'))
 }
 function font(){
-    return src("app/font/**/*")
+    return src('app/*font/**/*')
     .pipe(fontmin())
-    .pipe(dest("app/font"))
+    .pipe(dest("dist/font"))
 }
-exports.default = parallel(watching, style, script, browsersync)
-exports.build = series(del, font, image, build);
+exports.default = parallel(watching, scssCompile, browsersync);
+exports.build = series(del, font, image, scriptBuild, styleBuild, html, build);
